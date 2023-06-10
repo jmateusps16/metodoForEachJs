15a33a3b0648918d03078bbc57a4a9cefeb947a5
Mateus Palmeira <jmateusps16@gmail.com>
2023-06-10 17:13:39 -0300

    usando map para criar um novo array com novos valores

---
+++ app/metodoMap.js
@@ -0,0 +1,7 @@
+function aplicarDesconto(listaDeLivros){
+    const desconto = 0.3;
+    livrosComDesconto = listaDeLivros.map(livro => {
+        return {...livro, preco: livro.preco - (livro.preco * desconto)}
+    })
+    return livrosComDesconto;
+}
