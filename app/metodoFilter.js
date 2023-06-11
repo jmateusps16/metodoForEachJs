05b9a69b978f2448e0b7e0bb9aff6e1b26cc007a
Mateus Palmeira <jmateusps16@gmail.com>
2023-06-10 23:57:40 -0300

    aplicando filtro de disponibilidade

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,5 +1,6 @@
 // const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
 // btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront);
+let apenasDisponivel = false;
 
 const botoes = document.querySelectorAll('.btn');
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
@@ -7,8 +8,17 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const livroCategoria = elementoBtn.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria == livroCategoria);
+    let livrosFiltrados = livroCategoria == 'disponivel' ? filtraDisponibilidade(livros) : livros.filter(livro => livro.categoria == livroCategoria);
     if (livroCategoria != null && livroCategoria.trim() !== '') {
         exibirOsLivrosNaTela(livrosFiltrados);
+    }
+}
+
+function filtraDisponibilidade(livros) {
+    apenasDisponivel = !apenasDisponivel;
+    if (apenasDisponivel) {
+        return livros.filter(livro => livro.quantidade > 0);
+    } else {
+        return livros;
     }
 }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,10 +3,12 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livros__imagens indisponivel';
         elementoParaInserirLivros.innerHTML += 
         `
             <div class="livro">
-                <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+                <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
                 <h2 class="livro__titulo">${livro.titulo}</h2>
                 <p class="livro__descricao">${livro.autor}</p>
                 <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
@@ -17,3 +19,11 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         `
     })
 }
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//     if (livro != null && livro.quantidade > 0) {
+//         return 'livro__imagens';
+//     } else {
+//         return 'livros__imagens indisponivel'
+//     }
+// }
